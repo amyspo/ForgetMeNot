9e694d38a7b567688b13a374c53c0ef220844c0e
Amanda Sporrer <amanda.sporrer@gmail.com>
2026-05-13 14:08:59 +0200

    Changed Tasks to be Objects

--- src/components/TaskInput/TaskInput.jsx
+++ src/components/TaskInput/TaskInput.jsx
@@ -2,29 +2,104 @@ import React from 'react';
 import styles from './TaskInput.module.css';
 
 function TaskInput({ addTask }) {
-  const [inputValue, setInputValue] =
-    React.useState('');
+  const [task, setTask] = React.useState({
+    taskTitle: 'Do Laundry',
+    taskDescription: 'Sort laundry and wash it',
+    dueNext: '2026-05-15',
+    scheduledAs: 'monthly',
+  });
+
+  console.log(task)
+
+  function handleTitleChange(e) {
+    setTask({
+      ...task,
+      taskTitle: e.target.value
+    });
+  }
+
+  function handleDescriptionChange(e) {
+    setTask({
+      ...task,
+      taskDescription: e.target.value
+    });
+  }
+
+  function handleDueNext(e) {
+    setTask({
+      ...task,
+      dueNext: e.target.value
+    });
+  }
+
+  function handleScheduledAs(e) {
+    setTask({
+      ...task,
+      scheduledAs: e.target.value
+    });
+  }
   
   return <div>
     <form
       className="taskinput"
       onSubmit={event => {
         event.preventDefault();
-        addTask(inputValue);
-        setInputValue('');
+        addTask(task);
+        setTask({
+          taskTitle: '',
+          taskDescription: '',
+          dueNext: '2026-05-15',
+          scheduledAs: 'daily',
+        });
       }}
     >
-      <label htmlFor='taskinput'>New Task: </label>
-      <input
-        id='taskinput'
-        value={inputValue}
-        type="text"
-        onChange={event => {
-          setInputValue(event.target.value);
-        }}
-      />
-      <button>Add Task</button>
-      </form>    
+      <div>
+      <label>New Task: 
+        <input
+        value={task.taskTitle}
+        onChange={handleTitleChange}
+        />
+      </label>
+      </div>
+      <div>
+      <label>Description:  
+        <textarea
+        value={task.taskDescription}
+        onChange={handleDescriptionChange}
+        />
+      </label>
+      </div>
+      <div>
+        <label>Schedule:
+          <select
+            value={task.scheduledAs}
+            onChange={handleScheduledAs}
+          >
+              <option value="daily">
+                daily
+              </option>
+              <option value="weekly">
+                weekly
+              </option>
+              <option value="monthly">
+                monthly
+              </option>
+              <option value="yearly">
+                yearly
+              </option>
+            </select>
+          </label>
+        </div>
+      <div>
+        <label>Next Due Date: 
+          <input
+          type='date'
+          value={task.dueNext}
+          onChange={handleDueNext}
+        /></label>
+      </div>
+      <button>Add Task</button>   
+      </form>
   </div>;
 }
 
--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 
-
 function TaskList({newTask}) {
   return <div>
     <ul>
       {newTask.map((task) => (
-        <li key={Math.random()}>{task}</li>
+        <li key={Math.random()}>
+          <div>
+            <h3>{task.taskTitle}</h3> 
+            Due: {task.dueNext} Schedule: {task.scheduledAs}
+          </div>
+          <div>
+            Description: {task.taskDescription} 
+          </div>
+        </li>
       ))}
     </ul>
   </div>;
