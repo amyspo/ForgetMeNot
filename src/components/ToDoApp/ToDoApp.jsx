7d2fa46d41801be81dd45a34a792d4012ffd720d
Amanda Sporrer <amanda.sporrer@gmail.com>
2026-05-22 14:41:56 +0200

    deleted some old logic

--- src/components/ToDoApp/ToDoApp.jsx
+++ src/components/ToDoApp/ToDoApp.jsx
@@ -4,18 +4,11 @@ import ToDoInput from '../ToDoInput';
 import ToDoList from '../ToDoList';
 
 function ToDoApp() {
-  const [toDoList, setToDoList] = React.useState([]);
-
-  function addToDo(newToDo) {
-    const newList = [...toDoList, newToDo];
-    setToDoList(newList);
-    // console.log(newList);
-  }
 
   return <div>
     <Header/>
-    <ToDoInput addToDo={addToDo}/>
-    <ToDoList toDoList={toDoList}/>
+    <ToDoInput/>
+    <ToDoList/>
   </div>;
 }
 
